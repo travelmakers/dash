3108e348189e5a90d421ad8ec9f1bd69a52ec311
Seong Gi Dong <sgd0947@gmail.com>
2023-03-28 17:12:49 +0900

    feat(token): radius 타입 변경 (number -> string)

--- packages/travelmakers-design-styles/src/theme/tokens/radius.ts
+++ packages/travelmakers-design-styles/src/theme/tokens/radius.ts
@@ -2,8 +2,8 @@ import { TmSize } from "../types/TmSize";
 
 export type CoRadius = "radius10" | "radius20" | "radius100";
 
-export const DEFAULT_RADIUS: Record<CoRadius, number> = {
-  radius10: 4,
-  radius20: 8,
-  radius100: 100,
+export const DEFAULT_RADIUS: Record<CoRadius, string> = {
+  radius10: "4px",
+  radius20: "8px",
+  radius100: "100px",
 };
