fc67955a988034b3be7bd012da5c2fa0f06472c1
Seong Gi Dong <sgd0947@gmail.com>
2023-04-03 14:23:22 +0900

    feat(button): crtead a ButtonIcon Component
    LH-359

--- packages/travelmakers-design-core/src/components/Button/ButtonIcon/ButtonIcon.tsx
+++ packages/travelmakers-design-core/src/components/Button/ButtonIcon/ButtonIcon.tsx
@@ -4,21 +4,28 @@ import {
   PolymorphicRef,
   TmComponentProps,
   TmSize,
+  useTmTheme,
 } from "@travelmakers-design-v2/styles";
 import { forwardRef, PropsWithChildren } from "react";
+import { Icon } from "../../Icon";
 import { View } from "../../View";
 import useStyles from "./ButtonIcon.style";
 
 export type ButtonIconStylesNames = ClassNames<typeof useStyles>;
 
 export interface Props {
-  /** Button 컴포넌트의 크기를 정합니다. */
-  size?: TmSize;
+  /** ButtonIcon 컴포넌트의 크기를 정합니다. */
+  size?: "small" | "medium";
 
-  /** true일 경우 Button이 disabled 됩니다. */
+  /** true일 경우 ButtonIcon이 disabled 됩니다. */
   disabled?: boolean;
 
+  /** ButtonIcon 컴포넌트의 색상을 정합니다. */
+  variant?: "primary" | "secondary";
+
   type: "prev" | "next";
+
+  buttonTheme?: "light" | "dark";
 }
 export interface SharedButtonIconProps
   extends Props,
@@ -37,25 +44,69 @@ export const ButtonIcon: ButtonIconComponent & {
   <C extends React.ElementType = "button">(
     {
       size,
+      variant = "primary",
       disabled,
       type,
+      buttonTheme = "dark",
       className,
       children,
       ...props
     }: PropsWithChildren<ButtonIconProps<C>>,
     ref: PolymorphicRef<C>
   ) => {
-    const { classes, cx } = useStyles({ size, disabled, type });
+    const { classes, cx } = useStyles({
+      size,
+      variant,
+      disabled,
+      type,
+      buttonTheme,
+    });
+    const theme = useTmTheme();
+
+    const getIconColor = (
+      buttonTheme: Props["buttonTheme"],
+      variant: Props["variant"],
+      disabled: Props["disabled"]
+    ) => {
+      if (variant === "secondary") {
+        if (disabled) {
+          if (buttonTheme === "dark") return theme.colors.primaryContainer;
+          if (buttonTheme === "light") return theme.colors.primaryContainer;
+        } else {
+          if (buttonTheme === "dark") return theme.colors.primary;
+          if (buttonTheme === "light") return theme.colors.white;
+        }
+      } else {
+        if (disabled) {
+          return theme.colors.white;
+        } else if (buttonTheme === "dark") {
+          return theme.colors.white;
+        } else if (buttonTheme === "light") {
+          return theme.colors.primary;
+        }
+      }
+    };
 
     return (
       <View<React.ElementType>
         component={"button"}
         ref={ref}
         type="button"
+        disabled={disabled}
         className={cx(className, classes.button)}
         {...props}
       >
-        {children}
+        {type === "prev" ? (
+          <Icon
+            src="IcAngleLeft"
+            color={getIconColor(buttonTheme, variant, disabled)}
+          />
+        ) : (
+          <Icon
+            src="IcAngleRight"
+            color={getIconColor(buttonTheme, variant, disabled)}
+          />
+        )}
       </View>
     );
   }
---
+++ packages/travelmakers-design-core/src/components/Button/ButtonIcon/stories/ButtonIcon.stories.tsx
@@ -0,0 +1,99 @@
+import {
+  ArgsTable,
+  Description,
+  PRIMARY_STORY,
+  Primary,
+  Stories,
+  Subtitle,
+  Title,
+} from "@storybook/addon-docs";
+
+import { ButtonIcon } from "../ButtonIcon";
+import { Meta } from "@storybook/react";
+import React from "react";
+
+export default {
+  title: "@travelmakers-design-v2/core/General/Button",
+  component: ButtonIcon,
+  argTypes: {
+    size: {
+      defaultValue: "small",
+      description: "ButtonIcon 컴포넌트의 크기를 정합니다.",
+      options: ["small", "medium"],
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+      control: { type: "inline-radio" },
+    },
+    variant: {
+      defaultValue: "primary",
+      description: "ButtonIcon 컴포넌트의 색상을 정합니다.",
+      options: ["primary", "secondary"],
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+      control: { type: "inline-radio" },
+    },
+    buttonTheme: {
+      defaultValue: "light",
+      description: "ButtonIcon 컴포넌트의 컬러테마를 정합니다.",
+      options: ["light", "dark"],
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+      control: { type: "inline-radio" },
+    },
+    disabled: {
+      defaultValue: false,
+      description: "true일 경우 ButtonIcon이 disabled 됩니다.",
+      table: {
+        type: {
+          summary: "boolean",
+        },
+      },
+      control: { type: "boolean" },
+    },
+    type: {
+      defaultValue: "prev",
+      description: "ButtonIcon의 화살표방향을 결정한다.",
+      options: ["prev", "next"],
+      table: {
+        type: {
+          summary: "string",
+        },
+      },
+      control: { type: "inline-radio" },
+    },
+  },
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <Subtitle />
+          <Description />
+          <Primary />
+          <ArgsTable story={PRIMARY_STORY} />
+          <Stories />
+        </>
+      ),
+    },
+    actions: {
+      handles: ["click button"],
+    },
+  },
+} as Meta;
+
+export const Icon = (props) => {
+  return (
+    <div style={{ margin: "0 auto" }}>
+      <ButtonIcon {...props}>ButtonIcon</ButtonIcon>
+    </div>
+  );
+};
