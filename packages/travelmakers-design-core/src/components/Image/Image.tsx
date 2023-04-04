2e11cf2732f38045518d6341ec09417d72aae585
Seong Gi Dong <sgd0947@gmail.com>
2023-04-04 18:01:40 +0900

    feat(image): 타입관련 부분 수정
    LH-393

--- packages/travelmakers-design-core/src/components/Image/Image.tsx
+++ packages/travelmakers-design-core/src/components/Image/Image.tsx
@@ -1,22 +1,14 @@
-"use client";
-
 import { PolymorphicRef } from "@travelmakers-design-v2/styles";
-import { forwardRef, PropsWithChildren, useState } from "react";
+import React, { forwardRef, useState } from "react";
 import useStyles from "./Image.style";
 import { ImageComponent, ImageProps } from "./Image.type";
 
-export interface Props {
+export interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
   /** true일 경우 lazy load가 적용됩니다. */
   lazy?: boolean;
 
   /** 이미지 src를 정합니다. */
   src: string;
-
-  /** Image 컴포넌트의 너비를 정합니다. */
-  width?: number | string;
-
-  /** Image 컴포넌트의 높이를 정합니다. */
-  height?: number | string;
 
   /** 이미지 설명을 추가합니다. */
   alt: string;
@@ -26,18 +18,10 @@ export const Image: ImageComponent & {
   displayName?: string;
 } = forwardRef(
   <C extends React.ElementType = "img">(
-    {
-      lazy = true,
-      src,
-      alt,
-      className,
-      children,
-      ...props
-    }: PropsWithChildren<ImageProps<C>>,
+    { lazy = true, src, alt, className, ...props }: ImageProps<C>,
     ref: PolymorphicRef<C>
   ) => {
     const [load, setLoad] = useState(false);
-    const [error, setError] = useState(false);
     const { classes, cx } = useStyles({ load });
 
     return (
@@ -51,17 +35,16 @@ export const Image: ImageComponent & {
           onLoad={() => setLoad(true)}
           onError={(e) => {
             setLoad(true);
-            setError(true);
             e.currentTarget.src = require("./img/error.png");
           }}
           {...props}
         />
         {!load && (
           <img
+            loading={lazy ? "lazy" : "eager"}
             src={
               "data:image/gif;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO8UA8AAiUBUcc3qzwAAAAASUVORK5CYII="
             }
-            alt={alt}
             {...props}
           />
         )}
