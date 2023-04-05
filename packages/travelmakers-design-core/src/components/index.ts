db6d29eb20a057f0fa6dc6f9db226d67fbbe3ed7
baegofda <baegofdao3o@gmail.com>
2023-04-05 14:39:07 +0900

    fix(index): export 누락 수정

--- packages/travelmakers-design-core/src/components/index.ts
+++ packages/travelmakers-design-core/src/components/index.ts
@@ -5,6 +5,8 @@ export * from "./Button";
 export * from "./Callout";
 export * from "./Icon";
 export * from "./Image";
+export * from "./Menu";
+export * from "./Price";
 export * from "./Tag";
 export * from "./Typography";
 export * from "./View";
