4f66c0263dd424a4c50d41c5f3cc9a848d4f34ac
baegofda <baegofdao3o@gmail.com>
2023-04-18 17:06:59 +0900

    feat(input): Input 컴포넌트 추가
    LH-367

---
+++ packages/travelmakers-design-core/src/components/Input/Input.style.ts
@@ -0,0 +1,91 @@
+import { TmColor, createStyles } from "@travelmakers-design-v2/styles";
+import { Props } from "./Input";
+
+const getBorderColor = (
+  colors: Record<TmColor, string>,
+  isError: Props["isError"],
+  color: string
+) => (isError ? colors.error : color);
+
+const getStatusStyle = (
+  colors: Record<TmColor, string>,
+  isError: Props["isError"],
+  defaultColor?: string
+) => {
+  if (isError === false) return colors.secondary1;
+  if (isError === true) return colors.error;
+  return defaultColor ?? "";
+};
+
+export default createStyles(
+  (
+    theme,
+    { subfix, isError }: { subfix?: Props["subfix"]; isError: Props["isError"] }
+  ) => {
+    const { typography, colors, spacing } = theme;
+
+    return {
+      root: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        rowGap: "4px",
+      },
+      focus: {
+        border: `1px solid ${getBorderColor(colors, isError, colors.primary)}`,
+      },
+      disabled: {
+        backgroundColor: colors.surface,
+      },
+      container: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        minWidth: "328px",
+        minHeight: "44px",
+        padding: `0 ${spacing.spacing30}`,
+        border: `1px solid ${getBorderColor(colors, isError, colors.outline)}`,
+        borderRadius: spacing.spacing10,
+        overflow: "hidden",
+      },
+      input: {
+        ...typography.body2,
+        padding: 0,
+        color: colors.primary2,
+        border: 0,
+        outline: "none",
+        width: subfix ? "100%" : "auto",
+
+        ["&::placeholder"]: {
+          color: colors.primary3,
+        },
+
+        ["&:focus"]: {
+          color: colors.primary1,
+          borderColor: getBorderColor(colors, isError, colors.primary),
+        },
+
+        ["&:disabled"]: {
+          color: colors.primary3,
+          backgroundColor: colors.surface,
+
+          ["&::placeholder"]: {
+            color: colors.primary4,
+          },
+        },
+      },
+      label: { ...typography.body2, color: colors.primary1 },
+      subfix: {
+        ...typography.body2,
+        paddingLeft: spacing.spacing30,
+        color: getStatusStyle(colors, isError, colors.primary3),
+        flexShrink: 0,
+      },
+      feedback: {
+        ...typography.body3,
+        color: getStatusStyle(colors, isError, colors.primary3),
+        fontWeight: 400,
+      },
+    };
+  }
+);
---
+++ packages/travelmakers-design-core/src/components/Input/Input.tsx
@@ -0,0 +1,97 @@
+import { PolymorphicRef } from "@travelmakers-design-v2/styles";
+import { forwardRef, useState } from "react";
+import { View } from "../View";
+import useStyles from "./Input.style";
+import { InputProps, InputComponent } from "./Input.type";
+
+export interface Props extends React.HTMLAttributes<HTMLInputElement> {
+  label?: string;
+  feedback?: string;
+  subfix?: string | number;
+  autoComplete?: HTMLInputElement["autocomplete"];
+  isError?: boolean;
+}
+
+let defaultId = 0;
+
+export const Input: InputComponent & {
+  displayName?: string;
+} = forwardRef(
+  <C extends React.ElementType = "input">(
+    {
+      label,
+      placeholder = "정보를 입력해주세요.",
+      feedback,
+      subfix,
+      isError = null,
+      autoComplete = "off",
+      className,
+      ...props
+    }: InputProps<C>,
+    ref: PolymorphicRef<C>
+  ) => {
+    const { classes, cx } = useStyles({ subfix, isError });
+    const [isFocus, setIsFocus] = useState(false);
+    const [id] = useState(() => String(defaultId++));
+    const elementId = `tm-input-${id}`;
+
+    const inputContent = () => {
+      if (subfix) {
+        return (
+          <div
+            className={cx(
+              classes.container,
+              isFocus && classes.focus,
+              props.disabled && classes.disabled
+            )}
+          >
+            <View<React.ElementType>
+              component={"input"}
+              className={classes.input}
+              id={elementId}
+              type={"text"}
+              ref={ref}
+              placeholder={placeholder}
+              autoComplete={autoComplete}
+              onClick={() => {
+                setIsFocus(true);
+              }}
+              onBlur={() => {
+                setIsFocus(false);
+              }}
+              {...props}
+            />
+            <div className={classes.subfix}>{subfix}</div>
+          </div>
+        );
+      }
+
+      return (
+        <View<React.ElementType>
+          component={"input"}
+          className={cx(classes.input, classes.container)}
+          id={elementId}
+          type={"text"}
+          ref={ref}
+          placeholder={placeholder}
+          autoComplete={autoComplete}
+          {...props}
+        />
+      );
+    };
+
+    return (
+      <div className={cx(classes.root, className)}>
+        {label && (
+          <label className={classes.label} htmlFor={elementId}>
+            {label}
+          </label>
+        )}
+        {inputContent()}
+        {feedback && <strong className={classes.feedback}>{feedback}</strong>}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
