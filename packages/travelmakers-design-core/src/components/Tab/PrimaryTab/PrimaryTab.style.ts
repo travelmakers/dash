7aff068ba584878406d249d595b59b216bf9da6a
baegofda <baegofdao3o@gmail.com>
2023-04-25 11:35:13 +0900

    fix(comment): 주석 제거

--- packages/travelmakers-design-core/src/components/Tab/PrimaryTab/PrimaryTab.style.ts
+++ packages/travelmakers-design-core/src/components/Tab/PrimaryTab/PrimaryTab.style.ts
@@ -1,12 +1,5 @@
 import { createStyles } from "@travelmakers-design-v2/styles";
-// import { Props } from "./PrimaryTab";
 
-/** style에서 사용할 Props Type을 선언합니다.
- * ex)
- *		export default createStyles((theme, { type }: Pick<Props, "prop">) => {
- *			// writhing styles
- *		}
- * */
 export default createStyles((theme) => {
   return {
     root: { display: "flex", margin: 0, padding: 0 },
