80b1d3b895c7f4468c62c4c174da47ff1a7e6a12
Seong Gi Dong <sgd0947@gmail.com>
2023-09-20 15:34:30 +0900

    fix(calendar): handleCalendar 로직에 대해서 원상복구

--- packages/travelmakers-design-core/src/components/CalendarSecond/Calendar.tsx
+++ packages/travelmakers-design-core/src/components/CalendarSecond/Calendar.tsx
@@ -134,7 +134,7 @@ export const CalendarSecond = forwardRef(
         actions.clear(selectableDates, disabledDays);
         handleCalendar();
       }
-    }, [type]);
+    }, []);
 
     return (
       <View<React.ElementType> component={"div"} className={cx(classes.root)}>
