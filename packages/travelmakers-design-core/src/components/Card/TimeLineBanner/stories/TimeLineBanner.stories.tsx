fb65a53cc7ac258b47e34dfe195ed3c21776a421
Seong Gi Dong <sgd0947@gmail.com>
2024-02-21 13:45:00 +0900

    feat(card): timeLineBanner에 대한 상태값 한번에 확인

--- packages/travelmakers-design-core/src/components/Card/TimeLineBanner/stories/TimeLineBanner.stories.tsx
+++ packages/travelmakers-design-core/src/components/Card/TimeLineBanner/stories/TimeLineBanner.stories.tsx
@@ -1,14 +1,15 @@
 import {
   ArgsTable,
   Description,
-  PRIMARY_STORY,
   Primary,
+  PRIMARY_STORY,
   Stories,
   Subtitle,
   Title,
 } from "@storybook/addon-docs";
 
 import { Meta } from "@storybook/react";
+import { getTimeLineFunc, getTimeLineProps } from "@travelmakers/utils";
 import React from "react";
 import { TimeLineBanner } from "../TimeLineBanner";
 
@@ -84,12 +85,126 @@ export default {
   },
 } as Meta;
 
+type ReservationState =
+  | "default"
+  | "tour_confirm_before"
+  | "tour_confirm"
+  | "tour_done"
+  | "reservation_purchase_before"
+  | "reservation_purchase_done"
+  | "checkin_before"
+  | "day_n"
+  | "extend_purchase_before"
+  | "extend_purchase_done"
+  | "extend_checkin_before"
+  | "reservation_change_process"
+  | "checkout_before"
+  | "checkout_before_n"
+  | "checkout_done"
+  | "tour_cancel"
+  | "reservation_cancel";
+
+const statueBanner = (state: ReservationState, locale: string) => {
+  // @ts-ignore
+  const timeLineFunc = getTimeLineFunc(state, locale);
+  const timeLineProps = getTimeLineProps(state, {
+    first: {
+      userName: "홍길동",
+      hotelName:
+        locale === "ko"
+          ? "서울드래곤시티 - 이비스 스타일 앰배서더 서울 용산"
+          : "SEOUL DRAGON CITY - ibis Styles Ambassador Seoul Yongsan",
+    },
+    second: {
+      userName: "홍길동",
+      hotelName:
+        locale === "ko"
+          ? "서울드래곤시티 - 이비스 스타일 앰배서더 서울 용산"
+          : "SEOUL DRAGON CITY - ibis Styles Ambassador Seoul Yongsan",
+      expectedDate: "2024-02-09T08:11:52.000000Z",
+      startDate: "2024-02-09T08:11:52.000000Z",
+      endDate: "2024-02-09T08:11:52.000000Z",
+      dDay: 4,
+    },
+    buttonText: {
+      expectedDate: "2024-02-09T08:11:52.000000Z",
+      endDate: "2024-02-09T08:11:52.000000Z",
+    },
+  });
+  const status = timeLineFunc.color;
+  const title = timeLineFunc.firstLineText(timeLineProps.first);
+  // @ts-ignore
+  const message = timeLineFunc.secondLineText(timeLineProps.second);
+  // @ts-ignore
+  const label = timeLineFunc.buttonText(timeLineProps.buttonText);
+  const arrowHidden = !timeLineFunc.link("/").arrow;
+  return { status, title, message, label, arrowHidden };
+};
+
 export const Default = (props) => {
+  const status: ReservationState[] = [
+    "default",
+    "tour_confirm_before",
+    "tour_confirm",
+    "tour_done",
+    "reservation_purchase_before",
+    "reservation_purchase_done",
+    "checkin_before",
+    "day_n",
+    "extend_purchase_before",
+    "extend_purchase_done",
+    "extend_checkin_before",
+    "reservation_change_process",
+    "checkout_before",
+    "checkout_before_n",
+    "checkout_done",
+    "tour_cancel",
+    "reservation_cancel",
+  ];
+
   return (
     <>
-      <div style={{ padding: 24, width: 328 }}>
-        <TimeLineBanner {...props} />
-      </div>
+      {status.map((state) => {
+        return (
+          <div style={{ padding: 24, width: 328 }}>
+            {state}: <TimeLineBanner {...statueBanner(state, "ko")} />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export const English = (props) => {
+  const status: ReservationState[] = [
+    "default",
+    "tour_confirm_before",
+    "tour_confirm",
+    "tour_done",
+    "reservation_purchase_before",
+    "reservation_purchase_done",
+    "checkin_before",
+    "day_n",
+    "extend_purchase_before",
+    "extend_purchase_done",
+    "extend_checkin_before",
+    "reservation_change_process",
+    "checkout_before",
+    "checkout_before_n",
+    "checkout_done",
+    "tour_cancel",
+    "reservation_cancel",
+  ];
+
+  return (
+    <>
+      {status.map((state) => {
+        return (
+          <div style={{ padding: 24, width: 328 }}>
+            {state}: <TimeLineBanner {...statueBanner(state, "en")} />
+          </div>
+        );
+      })}
     </>
   );
 };
